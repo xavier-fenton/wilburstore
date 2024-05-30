0cc4f9ffd1cf8f526bc3625f5a8310ce86a35c7f
Xavier Fenton <xavierfenton99@gmail.com>
2024-05-30 13:42:37 +1200

    Setup: Create basic look close to desired design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content:{
-    relative: true, 
+  content: {
+    relative: true,
     files: [
-    "./assets/**/*.{js,ts,jsx,tsx,mdx,liquid}",
-    "./theme/**/*.{js,ts,jsx,tsx,mdx,liquid}",
-    "./sections/**/*.{js,ts,jsx,tsx,mdx,liquid}",
-    "./snippets/**/*.{js,ts,jsx,tsx,mdx,liquid}",
-    
-   
- 
-  ],},
+      './assets/**/*.{js,ts,jsx,tsx,mdx,liquid}',
+      './theme/**/*.{js,ts,jsx,tsx,mdx,liquid}',
+      './sections/**/*.{js,ts,jsx,tsx,mdx,liquid}',
+      './snippets/**/*.{js,ts,jsx,tsx,mdx,liquid}',
+    ],
+  },
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
